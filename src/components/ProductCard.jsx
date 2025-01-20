b88d61013f8665c02ccf90c9844657c456c10458
seetall <2016seetal@gmail.com>
2025-01-20 21:21:11 +0545

    product card

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// receive product information and color as props
+const ProductCard = ({ productInformation, color }) => {
+    return (
+        <>
+            <div class="card" style={{ width: '15rem' }}>
+                <span style={{ backgroundColor: color }} className="badge position-absolute top-0">{productInformation.productCategory}</span>
+                <img src={`http://localhost:5000/products/${productInformation.productImage}`} class="card-img-top" alt="..."  style= {{height: '15rem'}} />
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="card-title">{productInformation.productName}</h5>
+                        <h5 class="card-title text-danger">NPR. {productInformation.productPrice}</h5>
+                    </div>
+                    <p class="card-text">{productInformation.productDescription.slice(0, 30)}</p>
+                    <a href="#" class="btn btn-outline-dark w-100">View More</a>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default ProductCard
