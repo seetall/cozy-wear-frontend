3a21fe53bfdee2dcdc3dcc8daf0d50b67956e90c
seetall <2016seetal@gmail.com>
2025-01-22 21:21:31 +0545

    modified

--- src/App.js
+++ src/App.js
@@ -16,6 +16,13 @@ import Register from './pages/register/register';
 import UpdateProduct from './pages/admin/update_product/UpdateProduct';
 import AdminDashboard from './pages/admin/admin_dashboard/AdminDashboard';
 import AdminRoutes from './pages/protected_routes/AdminRoutes';
+import ProductDetail from './pages/product_detail/ProductDetail';
+import Quiz from './pages/quiz/Quiz';
+import QuizResults from './pages/quiz/QuizResult';
+import UpdateForm from './pages/update_info/UpdateForm';
+import Esewa from './Esewa/Esewa';
+import BuyNowPage from './buy_now/BuyNowPage';
+import Checkout from './checkout/Checkout';
 
 
 
@@ -32,7 +39,16 @@ function App() {
           <Route path='/' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/dashboard' element={<UserDashboard />} />
-          {/* <Route path='/product/:id' element={<ProductDetails />} /> */}
+          <Route path='/product/:id' element={<ProductDetail />} />
+          <Route path='/quiz' element={<Quiz />} />
+          <Route path='/quiz-results?color=${answers.color}&style=${answers.style}&size=${answers.size}/' element={<QuizResults />} />
+          <Route path='/profile' element={<UpdateForm />} />
+          <Route path="/buy_now/:id" element={<BuyNowPage />} />
+          <Route path="/esewa" element={<Esewa />} />
+          <Route path="/checkout" element={<Checkout />} />
+
+
+
 
 
           {/* admin */}
